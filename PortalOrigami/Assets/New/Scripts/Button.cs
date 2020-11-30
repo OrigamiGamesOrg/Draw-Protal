7f3812bf466a1b98ccce7167e24dcd0585790e23
Preet-Sra <preetsra.77999@gmail.com>
2020-11-30 15:43:21 +0530

    button

---
+++ PortalOrigami/Assets/New/Scripts/Button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject ElectricWall;
+    public bool TeleprotableButton;
+    public Vector3 Destination;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (TeleprotableButton)
+            {
+                ElectricWall.transform.position = Destination;
+            }
+            else
+            {
+                ElectricWall.SetActive(false);
+            }
+        }
+    }
+}
