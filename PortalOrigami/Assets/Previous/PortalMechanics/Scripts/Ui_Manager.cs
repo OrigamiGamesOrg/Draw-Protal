3e4781caa5177ad610b16cbc4068bd8ab0e5cfe7
ColossalMonk <17siddhartha.satyarthi@gmail.com>
2020-12-04 00:16:48 +0530

    meeting changes

--- PortalOrigami/Assets/Previous/PortalMechanics/Scripts/Ui_Manager.cs
+++ PortalOrigami/Assets/Previous/PortalMechanics/Scripts/Ui_Manager.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Ui_Manager : MonoBehaviour
 {
-    public GameObject InGame,Pause,GameOver;
+    public GameObject InGame, Pause, GameOver;
+    private Slider keybar;
+    private int keypresent;
 
+    private void Start()
+    {
+        keypresent = GameObject.FindGameObjectsWithTag("Key").Length;
+        keybar = FindObjectOfType<Slider>();
+        keybar.maxValue = keypresent;
+    }
+
+    private void Update()
+    {
+        int numberofkeyleft = GameObject.FindGameObjectsWithTag("Key").Length;
+        int amount = keypresent - numberofkeyleft;
+        keybar.value = amount;
+    }
     public void PausePanel()
     {
         InGame.SetActive(!InGame.activeSelf);
         Pause.SetActive(!Pause.activeSelf);
         GameOver.SetActive(false);
     }
-    public  void GameOverPanel()
+    public void GameOverPanel()
     {
         InGame.SetActive(false);
         Pause.SetActive(false);
