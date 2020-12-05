27ed5700e91bb9bc33fdf2109075863bd72d3f0d
ColossalMonk <17siddhartha.satyarthi@gmail.com>
2020-12-05 12:06:48 +0530

    key button

--- PortalOrigami/Assets/Previous/PortalMechanics/Scripts/GoldKey.cs
+++ PortalOrigami/Assets/Previous/PortalMechanics/Scripts/GoldKey.cs
@@ -7,12 +7,20 @@ public class GoldKey : MonoBehaviour
     public bool isKeyCollected = false;
 
     public GameObject collectionEffect;
+    public GameObject button;
+
 
     private void OnTriggerEnter(Collider other)
+    {
+        button.SetActive(true);
+    }
+
+    public void KeyCollection()
     {
         GameObject.FindGameObjectWithTag("Door").GetComponent<Door>().RemoveKey(this.gameObject);
         Destroy(gameObject);
         GameObject effect = Instantiate(collectionEffect, transform.position, Quaternion.identity);
         Destroy(effect, 2f);
+        button.SetActive(false);
     }
 }
